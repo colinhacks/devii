6b908b63be17f261ab69cc8fa572e09eb506888b
David Wu <dwu@live.ca>
2021-03-05 17:45:56 -0800

    build changes

--- globals.ts
+++ globals.ts
@@ -1,11 +1,11 @@
 export namespace globals {
-  export const yourName = 'Alyssa P. Hacker';
-  export const siteName = 'My Awesome Blog';
+  export const yourName = 'Jeanmarie Jackman';
+  export const siteName = 'Blog';
   export const siteDescription = 'I write about code \'n stuff';
   export const siteCreationDate = 'March 3, 2020 04:00:00 GMT';
-  export const twitterHandle = '@alyssaphacker';
-  export const email = 'alyssa@example.com';
-  export const url = 'https://alyssaphacker.com';
-  export const accentColor = '#4fc2b4';
+  export const twitterHandle = '@sweetjuanita512';
+  export const email = 'tbd';
+  export const url = 'https://blog.tlgger.com';
+  export const accentColor = '#8f02b4';
   export const googleAnalyticsId = ``; // e.g. 'UA-999999999-1'
 }
