1f28d348c434efea358f7c1a19856dfeb16649cb
Andrew Hurle <fracture91@gmail.com>
2021-04-27 12:16:55 -0400

    Make syntax highlighting work when client JS is disabled
    PrismLight will have access to all syntaxes on the server side and provide
    syntax-highlighted code blocks in HTML.  PrismAsyncLight will download
    only the necessary syntaxes on the client to hydrate the code blocks.
    
    This seems to work fine right here, but I noticed some server/client
    mismatches in dev on my own site.  There are PRs up to fix those problems:
    
    https://github.com/react-syntax-highlighter/react-syntax-highlighter/pull/361
    https://github.com/react-syntax-highlighter/react-syntax-highlighter/pull/362

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import darcula from 'react-syntax-highlighter/dist/cjs/styles/prism/darcula';
-import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismLight, PrismAsyncLight } from "react-syntax-highlighter"
+
+const SyntaxHighlighter =
+  typeof window === "undefined" ? PrismLight : PrismAsyncLight
 
 export default class Code extends React.PureComponent<{
   language: string;
