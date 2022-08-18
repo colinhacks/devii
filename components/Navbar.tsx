8d0e4b74cb25cb8635f8172fac9a2eba7792912b
tsioryRaman <rtsiory4@gmail.com>
2022-08-18 18:29:28 +0300

    fix:add Icon to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+
+import { Center, Flex } from '@chakra-ui/react'
+import { IconLink } from "./IconLink";
 export const Navbar: React.FC = () => {
     return (
         <React.Fragment>
+            <Flex bg={"radial-gradient( circle farthest-corner at 1.3% 2.8%, rgb(255, 255, 255,.5) 0%, rgb(237, 237 ,237,.5) 100.2%)"} justifyContent={"center"} flexDirection={"column"} width={'100px'} position={"fixed"} left={"0"} bottom={0} top={0} alignItems={"center"}>
+                <IconLink label={"home"} />
 
+
+            </Flex>
         </React.Fragment>
     );
 }
